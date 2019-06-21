36aed2c0f079f81a142e183795803212db39b0ea
nanoblit <nanoblit@users.noreply.github.com>
2019-06-21 11:33:28 +0200

    Adding smurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,12 +2,14 @@ import axios from 'axios';
 
 import * as types from './actionTypes';
 
+export const setSmurfs = smurfs => ({ type: types.GET_SMURFS, payload: smurfs });
+
 export const getSmurfs = () => dispatch => {
   dispatch({ type: types.START_GETTING_SMURFS });
   axios
     .get('http://localhost:3333/smurfs')
     .then(res => {
-      dispatch({ type: types.GET_SMURFS, payload: res.data });
+      dispatch(setSmurfs(res.data));
     })
     .catch(error => console.log(error.message))
     .then(() => dispatch({ type: types.FINISH_GETTING_SMURFS }));
@@ -17,8 +19,8 @@ export const addSmurf = (name, age, height) => dispatch => {
   dispatch({ type: types.START_ADDING_SMURF });
   axios
     .post('http://localhost:3333/smurfs', { name, age, height })
-    .then(() => {
-      getSmurfs();
+    .then(res => {
+      dispatch(setSmurfs(res.data));
     })
     .catch(error => console.log(error.message))
     .then(() => dispatch({ type: types.FINISH_ADDING_SMURF }));
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,14 +2,10 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import * as actions from '../actions';
+import SmurfForm from './SmurfForm';
 import SmurfList from './SmurfList';
 import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own.
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+
 const App = ({ getSmurfs }) => {
   useEffect(() => {
     getSmurfs();
@@ -17,6 +13,7 @@ const App = ({ getSmurfs }) => {
 
   return (
     <div className="App">
+      <SmurfForm />
       <SmurfList />
     </div>
   );
