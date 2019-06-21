e912df403bc07edd0522a2d6b17780bafb687953
nanoblit <nanoblit@users.noreply.github.com>
2019-06-21 10:07:47 +0200

    Add types and the reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,36 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+import * as types from './actionTypes';
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: types.START_GETTING_SMURFS });
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => {
+      dispatch({ type: types.GET_SMURFS, payload: res.data });
+    })
+    .catch(error => console.log(error.message))
+    .then(() => dispatch({ type: types.FINISH_GETTING_SMURFS }));
+};
+
+export const addSmurf = (name, age, height) => dispatch => {
+  dispatch({ type: types.START_ADDING_SMURF });
+  axios
+    .post('http://localhost:3333/smurfs', { name, age, height })
+    .then(() => {
+      getSmurfs();
+    })
+    .catch(error => console.log(error.message))
+    .then(() => dispatch({ type: types.FINISH_ADDING_SMURF }));
+};
+
+// export const updateSmurf = (id, name, age, height) => dispatch => {
+//   axios
+//     .get('http://localhost:3333/smurfs')
+//     .then(res => {
+//       dispatch({ type: types.GET_SMURFS, payload: res.data });
+//     })
+//     .catch(error => console.log(error.message));
+// };
+
+// export const deleteSmurf = id => dispatch => {};
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,4 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import * as types from '../actions/actionTypes';
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -13,10 +11,35 @@
    error: null
  }
 */
+const initialState = {
+  smurfs: [],
+  gettingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null,
+};
+
+export const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.GET_SMURFS:
+      return { ...state, smurfs: action.payload };
+    case types.START_GETTING_SMURFS:
+      return { ...state, gettingSmurf: true };
+    case types.FINISH_GETTING_SMURFS:
+      return { ...state, gettingSmurf: false };
+    case types.START_ADDING_SMURF:
+      return { ...state, addingSmurf: true };
+    case types.FINISH_ADDING_SMURF:
+      return { ...state, addingSmurf: false };
+    default:
+      return state;
+  }
+};
 
 /*
   You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
+  Feel free to export it as a default and import as rootReducer.
   This will guard your namespacing issues.
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
